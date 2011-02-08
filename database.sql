cdf7afd92a44b10de22a360f2e1468e568c65b0e
Nido Media <nido@foxserver.be>
2011-02-08 23:44:59 +0100

    database edit + add media removal

--- database.sql
+++ database.sql
@@ -4,6 +4,6 @@ create table if not exists files ( sha512 char(129), path varchar(255) primary k
 create table if not exists media ( name varchar(255) primary key, media_type varchar(16) );
 create table if not exists media_files ( media varchar(255) references media.name, files varchar(255) references files.path );
 grant select, insert, delete on mis.* to 'mis'@'localhost' identified by 'password';
-
+flush privileges
 
 --%insert into files values('$SHA512', '$FILE', true)
