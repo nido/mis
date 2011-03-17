9e2ac7990a0a6b813fb80dffb1407c66fd577a7f
Nido Media <nido@foxserver.be>
2011-03-17 01:23:47 +0100

    renaming stuff

---
+++ test_command.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""This unit tests the mis mysql module. It is a wrapper around
+the real mysql databse and thus this should also test the database
+itself."""
+from unittest import TestCase
+from unittest import main
+
+from commands import get_function
+from commands import get_command
+from commands import get_local_file
+
+class TestCommandModule(TestCase): # pylint: disable-msg=R0904
+    """Tests the commands module in mis"""
+    def setUp(self): # pylint: disable-msg=C0103
+        pass
+
+    def test_get_function(self):
+        guess = get_function('get filedataargument')
+        answer = (get_local_file, 'argument')
+        self.assertTrue(guess == answer)
+
+# TODO: I need a more proper test database for this one
+#    def test_find_container(self):
+#        find_container(nodename, path)
+
+if __name__ == '__main__':
+    main()
+
+# vim: set tabstop=4 expandtab textwidth=66: #
