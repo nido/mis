6f1b3d5563d8d119d2d690f408b8d38d1769c8c7
Nido Media <nido@foxserver.be>
2013-04-20 19:49:57 +0200

    add autoconfiguration, delete legacy

--- legacy/create_loglevel_defaults.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# simple script generating the loglevel defaults creation in python. For devs only
-cat *.py | grep -Go "'mis\.[^']*'" | sort | uniq | while read line; do echo "    config.set($line, 'info')"; done
